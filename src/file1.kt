74fc19bffa954bc7ce9a89392b58a1b731adfdc4
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 21:09:24 +0530

    Initial commit

---
+++ src/file1.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>) {
+    val i = 'A'
+    val ascii = i.toInt()
+    println("The ASCII value of i is " +ascii)
+
+    println("No:8 Kotline Prog , By Sanjay D Shirsath")
+}
